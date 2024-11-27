1ff860eb0da031e1713634406c02ddb6e04d1146
Mirsakina <xankisiyevamirsakina@gmail.com>
2024-11-27 22:05:40 +0400

    init file added

---
+++ task4/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data);
+        setLoading(false);
+      })
+  },); 
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li> 
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
